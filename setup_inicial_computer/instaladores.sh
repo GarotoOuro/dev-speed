9dd179c9268d4a13f536b29ca71136bbddd1a950
GarotoOuro <ggs.arquivo.2030@gmail.com>
2025-07-05 05:19:02 +0000

    update

--- setup_inicial_computer/instaladores.sh
+++ setup_inicial_computer/instaladores.sh
@@ -13,13 +13,6 @@ sudo apt install tree
 sudo apt install --no-install-recommends xserver-xorg xinit i3
 sudo apt install -y alacritty
 
-curl -Lo vivaldi.deb "https://downloads.vivaldi.com/stable/vivaldi-stable_6.8.3381.44-1_amd64.deb"
-
-# sudo apt install firefox-esr
-source ./scripts/neovim_installer
-
-
-
 
 echo SUCESS THE END
 
