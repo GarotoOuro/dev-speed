a5472b0aab87cda5ecf9f4ee2b044094e8de38c7
GarotoOuro <ggs.arquivo.2030@gmail.com>
2025-07-05 04:22:49 +0000

    default

--- setup_inicial_computer/installer.sh
+++ setup_inicial_computer/installer.sh
@@ -1,5 +1,10 @@
 sudo apt update && sudo apt upgrade -y
 
+sudo apt install -y dash
+sudo dpkg-reconfigure dash
+ls -l /bin/sh
+chsh -s /bin/dash
+
 sudo apt install -y git
 sudo apt install -y gh
 
@@ -10,11 +15,7 @@ sudo apt install -y alacritty
 sudo apt install firefox-esr
 source ./scripts/neovim_installer
 
-sudo apt install -y dash
-sudo dpkg-reconfigure dash
-ls -l /bin/sh
-chsh -s /bin/dash
-sudo apt remove --purge bash
+
 
 
 echo SUCESS THE END
