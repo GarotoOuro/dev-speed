5ff38933a7170e30206317ab46119db085d2b10b
GarotoOuro <ggs.arquivo.2030@gmail.com>
2025-07-05 01:32:59 +0000

    default

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dev-speed
 
+
+startx
---
+++ installer.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install tree
+
+sudo apt install --no-install-recommends xserver-xorg xinit
+
+sudo apt install -y alacritty
+
+sudo apt install firefox-esr
+
+
+
